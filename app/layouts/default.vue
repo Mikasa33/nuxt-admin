413d61f5930e838462578a35613fbe2b38380a09
Mikasa33 <mikasa33@qq.com>
2025-01-05 22:11:42 +0800

    feat: 开发布局

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,3 +1,43 @@
+<script lang="ts" setup>
+const { collapsed } = storeToRefs(useAppStore())
+</script>
+
 <template>
-  <NuxtPage />
+  <NLayout class="h-100vh w-full">
+    <NLayoutHeader class="h-64px">
+      <div class="h-full flex">
+        <LayoutLogo />
+        <div class="flex-1" />
+        <div class="mr-24px h-full flex-y-center gap-x-8px">
+          <LayoutGithub />
+          <LayoutTheme />
+          <LayoutUser />
+        </div>
+      </div>
+    </NLayoutHeader>
+    <NLayout
+      has-sider
+      position="absolute"
+      class="!top-64px"
+    >
+      <NLayoutSider
+        v-model:collapsed="collapsed"
+        collapse-mode="width"
+        :collapsed-width="64"
+        :native-scrollbar="false"
+        show-trigger="arrow-circle"
+        :width="240"
+      >
+        <LayoutMenu />
+      </NLayoutSider>
+      <NLayout
+        embedded
+        :native-scrollbar="false"
+        content-class="p-24px"
+        class="rounded-tl-12px"
+      >
+        <NuxtPage />
+      </NLayout>
+    </NLayout>
+  </NLayout>
 </template>
